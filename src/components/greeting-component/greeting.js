97c9ad922bb67febf618efbbe1279e1e1f60ebf1
Nickalis <nickalis1999@gmail.com>
2020-10-19 18:41:25 -0700

    init react portfolio

---
+++ src/components/greeting-component/greeting.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './greeting.css'
+
+function Gretting() {
+    return (
+        <div className="main-container">
+            <div className='greting-wapper'>
+                <h1>Hi, I'm Nickalis Salmons.</h1>
+            </div>
+            <div className="into-wrapper">
+                <div className="nav-wrapper">
+                    <ul>
+                        <div id="dot-1" class="browser-dot"></div>
+                        <div id="dot-2" class="browser-dot"></div>
+                        <div id="dot-3" class="browser-dot"></div>
+                    </ul>
+                    <ul id="navigation">
+
+                    </ul>
+                </div>
+            <div className="left-colum">
+
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Gretting;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Greeting from './components/greeting-component/greeting.js'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Greeting />
   </React.StrictMode>,
   document.getElementById('root')
 );
